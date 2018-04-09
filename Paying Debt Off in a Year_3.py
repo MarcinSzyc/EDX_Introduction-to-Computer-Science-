6cce4dcf8ee3d5fba0a5e334f1c0c3ca13907c1e
Marcinu85 <szyc.marcin@gmail.com>
2018-04-09 13:28:39 +0200

    Add files via upload

---
+++ Paying Debt Off in a Year_3.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  9 11:58:13 2018
+
+@author: Marcin
+"""
+balance = 320000
+annualInterestRate = 0.2
+previousBalance=0
+monthlyInterestRate = annualInterestRate / 12
+monthlyPaymentLowerBound = balance / 12
+monthlyPaymentUpperBound = (balance*((1 + monthlyInterestRate)**12))/12
+test=balance
+
+
+while test > 0.1:
+    if previousBalance < 0:
+        monthlyPaymentUpperBound = minimumMonthlyPayment
+    elif previousBalance >0:
+        monthlyPaymentLowerBound = minimumMonthlyPayment
+    previousBalance = balance
+    minimumMonthlyPayment = (monthlyPaymentUpperBound + monthlyPaymentLowerBound)/2
+    minimumMonthlyPayment = round(minimumMonthlyPayment,2)
+    for i in range(12):
+        monthlyUnpaidBalance = previousBalance - minimumMonthlyPayment
+        unpaidBalanceEachMonth = monthlyUnpaidBalance + (monthlyInterestRate*monthlyUnpaidBalance)
+        previousBalance = unpaidBalanceEachMonth
+    test = balance - (balance-abs(previousBalance))
+print('Lowest Payment: ' + str(minimumMonthlyPayment))
